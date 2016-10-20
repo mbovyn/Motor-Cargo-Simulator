0bbd523f1b79f8571bbed6e5236dccb4357bebda
Matt Bovyn <matt.bovyn@gmail.com>
2016-10-20 15:59:06 -0700

    Revert "Revert "removed code for integration with GSL  - will have GSL brach with that implemented and direct equation branch""
    Want to merge directEquations with master cleanly
    
    This reverts commit 434e9a85c6b8c8a77e8d0280d05675477df51e67.

--- driveMotors.c
+++ driveMotors.c
@@ -7,8 +7,6 @@
 #include <stdio.h>
 #include <string.h>
 #include "twister.c"
-//GNU scientific library
-#include <gsl/gsl_linalg.h>
 
 /*******************************************************************************/
 //  INCLUDES
@@ -99,8 +97,6 @@ int main( int argc, char *argv[] )
     if(verboseTF>0){
         printf("There were %d successes in %d trials\n",successes,repeats );
     }
-
-    do_linalg();
 
     return 0;
 
