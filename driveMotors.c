601dabf351d9548fbf1c09f735f2031b90452de0
Matt Bovyn <matt.bovyn@gmail.com>
2016-11-09 13:00:54 -0800

    Put back to using random seed

--- driveMotors.c
+++ driveMotors.c
@@ -52,7 +52,7 @@ int main( int argc, char *argv[] )
         verboseTF = atoi(argv[4]);
 
     // Intialize random number generator (twister.c)
-    RanInit(1);
+    RanInit(0); //can set RanInit(1) to use same seed every time
     if(verboseTF>0){
         //if ever see two that are the same, know iSEED wasn't updated
         printf("The test rand is %f\n",RAND);
