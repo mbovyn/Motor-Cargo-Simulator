b04d5c3d700a667cd1e3e5e8fa15dfd116a7aca5
Matt Bovyn <matt.bovyn@gmail.com>
2017-01-24 16:58:42 -0800

    Find number of processes with pgrep, now always gives accurate count of processes running

--- tools/archetypes/multiple run shell script.sh
+++ tools/archetypes/multiple run shell script.sh
@@ -2,7 +2,7 @@
 
 #define run name here
 code_dir=~/project_code/Motor_Freedom
-run_name=demo_mult
+run_name=TOW_fraction
 
 #number of processes we want to run at once
 numCores=4
@@ -11,11 +11,11 @@ numCores=4
 ctr1=0
 
 sweep=($(seq 5 5 25))
+#sweep=15
 
 echo "Running ${#sweep[@]} params"
 
-echo "started at:"
-date
+date +"    started at: %r on %F"
 
 #for each set of paramters
 for param in ${sweep[*]};
@@ -23,7 +23,7 @@ do
 
     #find the number of simulations currently running
     #need to subtract 1 because grep finds itself
-    numInstances=$(( $( ps | grep motors | wc -l ) - 1 ))
+    numInstances=$( pgrep motors | wc -l )
 	#echo "    starting, have $numInstances running"
 
     talk=1
@@ -31,7 +31,7 @@ do
 	while [ "$numInstances" -ge "$numCores" ] # number of cores to use
 	do
 
-		numInstances=$(( $( ps | grep motors | wc -l ) - 1))
+		numInstances=$( pgrep motors | wc -l )
 
         if [ "$talk" -eq "1" ]
         then
@@ -39,7 +39,7 @@ do
             let talk=0
         fi
 
-		sleep 1s
+		sleep .1s
 	done
     #when this exits, we are at less than the max number of sims
     #so launch a new one and check again
@@ -56,15 +56,15 @@ do
     fi
 
     #launch a simulation in the background by adding & after call
-    #motors.x          run_name       instance_Name     repeats  verbose D     eps_0  pi_0   z_MT_offset R N F_trap theta_c
-    $code_dir/motors.x "${run_name}" "${instance_name}" 10      1       0     0      $param 0           0         &
+    #motors.x          run_name       instance_Name     repeats  verbose D     eps_0  pi_0   z_MT_offset R       N      F_trap theta_c
+    $code_dir/motors.x "${run_name}" "${instance_name}" 10       1       0     0      $param                   &
     #sleep so as not to go before the ISEED is updated
     sleep .000001s
 
     #track where we are
     let ctr1++
-	echo "    ran $ctr1 of ${#sweep[@]}, param=$param at"
-    date
+	echo "ran $ctr1 of ${#sweep[@]}, param=$param at"
+    date +"    %r on %F"
 
 	sleep .01
 
@@ -73,22 +73,29 @@ done #first loop
 echo finished launching
 
 let talk=1
-while [ "$numInstances" -gt "0" ] # number of cores to use
+numInstances=$( pgrep motors | wc -l )
+while [ "$numInstances" -gt "0" ]
 do
 
-    numInstances=$(($( ps | grep motors | wc -l ) - 1))
+    numInstancesnew=$( pgrep motors | wc -l )
+
+    if [ "$numInstancesnew" -lt "$numInstances" ]
+    then
+        numInstances=$numInstancesnew
+        let talk=1
+    fi
 
     if [ "$talk" -eq "1" ]
     then
-        echo waiting for last $numInstances to finish
+        echo waiting for last $numInstances to finish at
+        date +"    %r on %F"
         let talk=0
     fi
 
-    sleep 1s
+    sleep .1s
 done
 
-echo "done at:"
-date
+date +"done at: %r on %F"
 #create a notification
 osascript -e 'display notification "Simulation Complete" with title "Done!"'
 
