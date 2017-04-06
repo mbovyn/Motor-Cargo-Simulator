fa33fd8a9e337876595e3d969441722e06379c09
Matt Bovyn <matt.bovyn@gmail.com>
2017-04-05 20:41:07 -0700

    Add script to copy necessary files into another folder to prep for HPC run

---
+++ tools/prepHPC.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+code_dir=~/project_code/Motor_Freedom
+working_dir=$("pwd")
+
+#copy code to current folder
+cp $code_dir/*.c .
+cp $code_dir/*.h .
+
+#get commit hash
+cd $code_dir
+git log -1 --pretty=format:%H > "CommitHash.txt"
+mv "CommitHash.txt" "$working_dir"
+cd "$working_dir"
