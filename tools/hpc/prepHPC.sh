dc29e42d36c0f8d52d93f604864948cc19e0b45b
Matt Bovyn <matt.bovyn@gmail.com>
2017-04-06 20:27:47 -0700

    Add transfer of Makefile

--- tools/hpc/prepHPC.sh
+++ tools/hpc/prepHPC.sh
@@ -6,6 +6,7 @@ working_dir=$("pwd")
 #copy code to current folder
 cp $code_dir/*.c .
 cp $code_dir/*.h .
+cp $code_dir/Makefile .
 cp -n $code_dir/tools/hpc/make_and_submit_Pubs.pl .
 
 #get commit hash
