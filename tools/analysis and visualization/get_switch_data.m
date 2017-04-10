0eb093b0aec6240548b03f21c5d076dd6c5dca42
Matt Bovyn <matt.bovyn@gmail.com>
2017-04-10 14:58:56 -0700

    Improve switch data read in for heatmaps

--- tools/analysis and visualization/get_switch_data.m
+++ tools/analysis and visualization/get_switch_data.m
@@ -1,36 +1,64 @@
+%possible exit conditions are:
+% 1: detach before intersection
+% 8: exit ToW zone on primary MT (pass)
+% 9: exit ToW zone on crossing MT (switch)
+% 13: detach after intersection on primary MT (pass)
+% 14: detach after intersection on crossing MT (switch)
+
+%ToW given by TOW_time>0 (numerically TOW_time>.1)
+
+theta=zeros(nruns(1),nruns(2));
+
+towed=cell(nruns(1),nruns(2));
+switched=cell(nruns(1),nruns(2));
+stayed_on=cell(nruns(1),nruns(2));
+fell_off=cell(nruns(1),nruns(2));
+discarded=cell(nruns(1),nruns(2));
+
+p_ToW=zeros(nruns(1),nruns(2));
+p_switch=zeros(nruns(1),nruns(2));
+p_switchToW=zeros(nruns(1),nruns(2));
+
+SE_ToW=zeros(nruns(1),nruns(2));
+SE_switch=zeros(nruns(1),nruns(2));
+SE_switchToW=zeros(nruns(1),nruns(2));
+
 for i=1:nruns(1)
     for j=1:nruns(2)
         
+        %find the angle between the MTs from their unit vectors
+        %(first MT always set to [1 0 0] unit vector, second MTvec only one
+        %passed out)
         theta(i,j)=180-acosd(dot(runs(i,j).MTvec(1,:),[1 0 0]));
 
-        %towed{j,i}=runs(i).success==1;
-        towed{i,j}=runs(i,j).success==1 | runs(i).exit_cond==9 | runs(i).exit_cond==14;
+        %ToW if
+        %ToW timer >0 or switch
+        towed{i,j}=runs(i,j).ToW_time>.1 | runs(i,j).exit_cond==9 | ...
+            runs(i,j).exit_cond==14;
+        %passed{i,j}=runs(i,j).exit_cond==8 | runs(i,j).exit_cond==13;
+        switched{i,j}=runs(i,j).exit_cond==9 | runs(i,j).exit_cond==14;
 
-        stayed_on{j,i}=runs(i).exit_cond==9 | runs(i).exit_cond==8 | runs(i).exit_cond==13 | runs(i).exit_cond==14;
-        discarded{j}(i)=sum(~stayed_on{j,i});
+        %was a valid run if have any of the listed exit conditions
+        stayed_on{i,j}=runs(i,j).exit_cond==9 | runs(i,j).exit_cond==8 | ...
+            runs(i,j).exit_cond==13 | runs(i,j).exit_cond==14;
+        fell_off{i,j}=runs(i,j).exit_cond==1;
+        discarded{i,j}=~fell_off{i,j} & ~stayed_on{i,j};
 
-        towandstay{j,i}=towed{j,i} & stayed_on{j,i};
-
-        TOWed{j}(i)=sum(towandstay{j,i});
-        passed{j}(i)=sum( ~towed{j,i} & stayed_on{j,i} );
-
-        fraction{j}(i)=TOWed{j}(i)/sum(stayed_on{j,i});
+        %find probability of ToW, switch, switch | ToW
+        n_stayed_on=sum(stayed_on{i,j});
+        p_ToW(i,j)=sum(towed{i,j} & stayed_on{i,j})/n_stayed_on;
+        p_switch(i,j)=sum(switched{i,j} & stayed_on{i,j})/n_stayed_on;
+        p_switchToW(i,j)=sum(towed{i,j} & switched{i,j} & stayed_on{i,j})/sum(towed{i,j});
 
         %SEM from 
         %http://stats.stackexchange.com/questions/29641/standard-error-for-the-mean-of-a-sample-of-binomial-random-variables
-
-        SEM{j}(i)=sqrt(fraction{j}(i)*(1-fraction{j}(i))/sum(stayed_on{j,i}));
-        
-        stayed_on=runs(i,j).exit_cond==12 | runs(i,j).exit_cond==8;
-        
-        TOWed{k}(i,j)=sum(runs(i,j).exit_cond(stayed_on)==12);
-        passed{k}(i,j)=sum(runs(i,j).exit_cond(stayed_on)==8);
-        
-        %fraction{1}(i,j)=TOWed{1}(i,j)/sum(stayed_on);
-        %SEM{1}(i,j)=sqrt(fraction{1}(i,j)*(1-fraction{1}(i,j))/sum(stayed_on{1}(i,j)));
-        
-        pi_0_axis{k}(i,j)=runs(i,j).pi_0(1);
-        N_axis{k}(i,j)=runs(i,j).N1(1);
+        SE_ToW(i,j)=sqrt(p_ToW(i,j)*(1-p_ToW(i,j))/n_stayed_on);
+        SE_switch(i,j)=sqrt(p_switch(i,j)*(1-p_switch(i,j))/n_stayed_on);
+        SE_switchToW(i,j)=sqrt(p_switchToW(i,j)*(1-p_switchToW(i,j))/n_stayed_on);
+    
+        %record parameters
+        pi_0_axis(i,j)=runs(i,j).pi_0(1);
+        N_axis(i,j)=runs(i,j).N1(1);
     
     end
 end
