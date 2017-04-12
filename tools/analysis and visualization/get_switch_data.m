9c0dd4776f40393aeee33263af5ae74d893872c1
Matt Bovyn <matt.bovyn@gmail.com>
2017-04-11 23:13:44 -0700

    Update heatmap making files

--- tools/analysis and visualization/get_switch_data.m
+++ tools/analysis and visualization/get_switch_data.m
@@ -15,6 +15,8 @@ stayed_on=cell(nruns(1),nruns(2));
 fell_off=cell(nruns(1),nruns(2));
 discarded=cell(nruns(1),nruns(2));
 
+n_stayed_on=zeros(nruns(1),nruns(2));
+n_ToWed=zeros(nruns(1),nruns(2));
 p_ToW=zeros(nruns(1),nruns(2));
 p_switch=zeros(nruns(1),nruns(2));
 p_switchToW=zeros(nruns(1),nruns(2));
@@ -45,16 +47,17 @@ for i=1:nruns(1)
         discarded{i,j}=~fell_off{i,j} & ~stayed_on{i,j};
 
         %find probability of ToW, switch, switch | ToW
-        n_stayed_on=sum(stayed_on{i,j});
-        p_ToW(i,j)=sum(towed{i,j} & stayed_on{i,j})/n_stayed_on;
-        p_switch(i,j)=sum(switched{i,j} & stayed_on{i,j})/n_stayed_on;
+        n_stayed_on(i,j)=sum(stayed_on{i,j});
+        n_ToWed(i,j)=sum(towed{i,j} & stayed_on{i,j});
+        p_ToW(i,j)=n_ToWed(i,j)/n_stayed_on(i,j);
+        p_switch(i,j)=sum(switched{i,j} & stayed_on{i,j})/n_stayed_on(i,j);
         p_switchToW(i,j)=sum(towed{i,j} & switched{i,j} & stayed_on{i,j})/sum(towed{i,j});
 
         %SEM from 
         %http://stats.stackexchange.com/questions/29641/standard-error-for-the-mean-of-a-sample-of-binomial-random-variables
-        SE_ToW(i,j)=sqrt(p_ToW(i,j)*(1-p_ToW(i,j))/n_stayed_on);
-        SE_switch(i,j)=sqrt(p_switch(i,j)*(1-p_switch(i,j))/n_stayed_on);
-        SE_switchToW(i,j)=sqrt(p_switchToW(i,j)*(1-p_switchToW(i,j))/n_stayed_on);
+        SE_ToW(i,j)=sqrt(p_ToW(i,j)*(1-p_ToW(i,j))/n_stayed_on(i,j));
+        SE_switch(i,j)=sqrt(p_switch(i,j)*(1-p_switch(i,j))/n_stayed_on(i,j));
+        SE_switchToW(i,j)=sqrt(p_switchToW(i,j)*(1-p_switchToW(i,j))/n_stayed_on(i,j));
     
         %record parameters
         pi_0_axis(i,j)=runs(i,j).pi_0(1);
--- tools/analysis and visualization/save_TOW_heatmap.m
+++ tools/analysis and visualization/save_TOW_heatmap.m
@@ -1,14 +1,37 @@
 
-set(findall(gcf,'-property','FontSize'),'FontSize',10)
+
 
 fig = gcf;
 fig.PaperUnits = 'centimeters';
 
-if(small)
-    fig.PaperPosition = [0 0 7.8 5.3];
-    fig.PaperSize = [7.8 5.3];
+%letter text width = 16.5cm
+%letter text height = 22.8cm
+
+w1=7.8;
+h1=7.4;
+
+% w2=w;
+% h2=2*h;
+
+w2=8;
+h2=6;
+
+if small
+    
+    set(findall(gcf,'-property','FontSize'),'FontSize',10)
+    
+    if(small==1)
+        fig.PaperPosition = [0 0 w1 h1];
+        fig.PaperSize = [w1 h1];
+    elseif small==2
+        fig.PaperPosition = [0 0 w2 h2];
+        fig.PaperSize = [w2 h2];
+    else
+        error('Bad value for small')
+    end
+    
+    print(fname,'-dpdf')
+    
 else
-    fig.PaperPosition = [0 0 8 11];
-    fig.PaperSize = [8 11];
+    printpdf(fname)
 end
-print(filename,'-dpdf')
