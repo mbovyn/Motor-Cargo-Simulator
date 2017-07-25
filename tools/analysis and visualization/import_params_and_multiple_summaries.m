e1b02e392be98fed0177b903f4379d66c83bca99
Matt Bovyn <matt.bovyn@gmail.com>
2017-07-24 19:22:58 -0700

    Consolidate summary variable definition

--- tools/analysis and visualization/import_params_and_multiple_summaries.m
+++ tools/analysis and visualization/import_params_and_multiple_summaries.m
@@ -5,44 +5,30 @@ if ~exist('params_imported','var')
     run([analysispath '/import_params.m'])
     %MT params
     run([analysispath '/import_MT_params.m'])
-    
+
     params_imported=true;
 end
 
 %take in the summary the simulation writes if we haven't yet
 if ~exist('summaries_imported','var')
-    
+
     base_run_name=run_name;
     runs=struct;
-    
+
     for runno=1:nruns
-        
+
         run_name=[base_run_name '.' int2str(runno-1)];
         run([analysispath '/import_summary.m'])
-        
-        runs(runno).repeat=repeat;
-        runs(runno).step = step;
-        runs(runno).t_final = t_final;
-        runs(runno).exit_cond = exit_cond;
-        runs(runno).success = success;
-        runs(runno).D_anchor = D_anchor;
-        runs(runno).eps_0 = eps_0;
-        runs(runno).pi_0 = pi_0;
-        runs(runno).offset = offset;
-        runs(runno).R = R;
-        runs(runno).N1 = N1;
-        runs(runno).F1 = F1;
-        runs(runno).theta_c = theta_c;
-        runs(runno).last_bound_center = last_bound_center;
-        runs(runno).center_final = center_final;
-        runs(runno).locs_final = locs_final;
-        runs(runno).head_final = head_final;
+
+        run([analysispath '/set_summary_vars.m'])
+        for varname=summary_vars
+            runs(runno).(varname{1})=transpose(eval(varname{1}));
+        end
         runs(runno).MTvec=MTvec{2};
-        runs(runno).ToW_time=ToW_time;
-        runs(runno).bound_final=bound_final;
+
     end
-    
-    clear runno    
+
+    clear runno
     summaries_imported=true;
-    
+
 end
--- tools/analysis and visualization/import_params_and_multiple_summaries2.m
+++ tools/analysis and visualization/import_params_and_multiple_summaries2.m
@@ -1,3 +1,5 @@
+%this version brings in parameters from 2D sweeps
+
 if ~exist('params_imported','var') && ~exist('param_file_sweep','var')
     %2 entry parameters
     run([analysispath '/import_params2.m'])
@@ -5,16 +7,16 @@ if ~exist('params_imported','var') && ~exist('param_file_sweep','var')
     run([analysispath '/import_params.m'])
     %MT params
     run([analysispath '/import_MT_params.m'])
-    
+
     params_imported=true;
 end
 
 %take in the summary the simulation writes if we haven't yet
 if ~exist('summaries_imported','var')
-    
+
     base_run_name=run_name;
     runs=struct;
-    
+
     run_name00=[base_run_name '.' int2str(0) '.' int2str(0)];
     filename00 = [localpath '/' run_name00 '_Summary.txt'];
     run_name01=[base_run_name '.' int2str(0) '.' int2str(1)];
@@ -34,11 +36,11 @@ if ~exist('summaries_imported','var')
     else
         error(['Unable to find file matching any format of ' filename00])
     end
-    
+
     for runno1=1:nruns(1)
-        
+
         for runno2=1:nruns(2)
-        
+
             if strcmp(format,'00')
                 run_name=[base_run_name '.' int2str(runno1-1) '.' int2str(runno2-1)];
             elseif strcmp(format,'01')
@@ -48,7 +50,7 @@ if ~exist('summaries_imported','var')
             elseif strcmp(format,'11')
                 run_name=[base_run_name '.' int2str(runno1) '.' int2str(runno2)];
             end
-            
+
             if exist('param_file_sweep','var') && ~exist('params_imported','var')
                 run([analysispath '/import_params2.m'])
                 run([analysispath '/import_params.m'])
@@ -69,39 +71,24 @@ if ~exist('summaries_imported','var')
                         'R_MT' , ...
                         'MTpt' , ...
                         'MTvec' }
-                    
+
                 runs(runno1,runno2).(varname{1})=transpose(eval(varname{1}));
-                
+
             end
-            
+
             run([analysispath '/import_summary.m'])
 
-            runs(runno1,runno2).repeat=repeat;
-            runs(runno1,runno2).step = step;
-            runs(runno1,runno2).t_final = t_final;
-            runs(runno1,runno2).exit_cond = exit_cond;
-            runs(runno1,runno2).success = success;
-            runs(runno1,runno2).D_anchor = D_anchor;
-            runs(runno1,runno2).eps_0 = eps_0;
-            runs(runno1,runno2).pi_0 = pi_0;
-            runs(runno1,runno2).offset = offset;
-            runs(runno1,runno2).R = R;
-            runs(runno1,runno2).N1 = N1;
-            runs(runno1,runno2).F1 = F1;
-            runs(runno1,runno2).theta_c = theta_c;
-            runs(runno1,runno2).last_bound_center = last_bound_center;
-            runs(runno1,runno2).center_final = center_final;
-            runs(runno1,runno2).locs_final = locs_final;
-            runs(runno1,runno2).head_final = head_final;
+            run([analysispath '/set_summary_vars.m'])
+            for varname=summary_vars
+                runs(runno1,runno2).(varname{1})=transpose(eval(varname{1}));
+            end
             runs(runno1,runno2).MTvec=MTvec{2};
-            runs(runno1,runno2).ToW_time=ToW_time;
-            runs(runno1,runno2).bound_final=bound_final;
-            
+
         end
     end
-    
+
     clear runno1 runno2
     run_name=base_run_name;
     summaries_imported=true;
-    
+
 end
