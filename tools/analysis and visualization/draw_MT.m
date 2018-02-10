ab9f9622b10c4ece927beca13d065326c70371e1
Matt Bovyn <matt.bovyn@gmail.com>
2018-02-09 21:19:05 -0800

    Get working for arbitrary plotting box and MT vector

--- tools/analysis and visualization/draw_MT.m
+++ tools/analysis and visualization/draw_MT.m
@@ -1,150 +1,42 @@
 function [ h_cyl,h_cap1,h_cap2 ] = draw_MT( xends,yends,zends,MTpt,MTvec,R_MT,varargin )
-%UNTITLED4 Summary of this function goes here
+%draw_MT Draw Microtubule for movie
 %   Detailed explanation goes here
 
-    if xends(1)<MTpt(1) && xends(2)>MTpt(1)
-        
-        if(MTvec(1)>0)
-        
-            % MTpt + n * MTvec = end
-            n1(1)=(xends(1)-MTpt(1))/MTvec(1);
-            n2(1)=(xends(2)-MTpt(1))/MTvec(1);
-            n1(2)=(yends(1)-MTpt(2))/MTvec(2);
-            n2(2)=(yends(2)-MTpt(2))/MTvec(2);
-            n1(3)=(zends(1)-MTpt(3))/MTvec(3);
-            n2(3)=(zends(2)-MTpt(3))/MTvec(3);
-            
-        else
-            
-            n1(1)=(xends(2)-MTpt(1))/MTvec(1);
-            n2(1)=(xends(1)-MTpt(1))/MTvec(1);
-            n1(2)=(yends(1)-MTpt(2))/MTvec(2);
-            n2(2)=(yends(2)-MTpt(2))/MTvec(2);
-            n1(3)=(zends(1)-MTpt(3))/MTvec(3);
-            n2(3)=(zends(2)-MTpt(3))/MTvec(3);
-        
-        end
+    %vector from MTpt to pass each plane defining the cube
+    v1=[max(xends) max(yends) max(zends)]-MTpt;
+    v2=[min(xends) min(yends) min(zends)]-MTpt;
 
-        end1=MTpt-MTvec.*min(abs(n1));
-        end2=MTpt+MTvec.*min(abs(n2));
-        
-    elseif xends(1)<MTpt(1) && xends(2)<MTpt(1)
-        
-        if(MTvec(1)>0)
-            
-            n1(1)=(xends(1)-MTpt(1))/MTvec(1);
-            n2(1)=(xends(2)-MTpt(1))/MTvec(1);
-            n1(2)=(yends(1)-MTpt(2))/MTvec(2);
-            n2(2)=(yends(2)-MTpt(2))/MTvec(2);
-            n1(3)=(zends(1)-MTpt(3))/MTvec(3);
-            n2(3)=(zends(2)-MTpt(3))/MTvec(3);
-            
-            end1=MTpt-MTvec.*min(abs(n1));
-            end2=MTpt-MTvec.*min(abs(n2));
-            
-        else
-            n1(1)=(xends(2)-MTpt(1))/MTvec(1);
-            n2(1)=(xends(1)-MTpt(1))/MTvec(1);
-            n1(2)=(yends(1)-MTpt(2))/MTvec(2);
-            n2(2)=(yends(2)-MTpt(2))/MTvec(2);
-            n1(3)=(zends(1)-MTpt(3))/MTvec(3);
-            n2(3)=(zends(2)-MTpt(3))/MTvec(3);
-            
-            end1=MTpt+MTvec.*min(abs(n1));
-            end2=MTpt+MTvec.*min(abs(n2));
-            
-        end
-        
-%     elseif xends(1)>MTpt(1) && xends(2)<MTpt(1)
-%         
-%         if(MTvec(1)>0)
-%             
-%             n1(1)=(xends(1)-MTpt(1))/MTvec(1);
-%             n2(1)=(xends(2)-MTpt(1))/MTvec(1);
-%             n1(2)=(yends(1)-MTpt(2))/MTvec(2);
-%             n2(2)=(yends(2)-MTpt(2))/MTvec(2);
-%             n1(3)=(zends(1)-MTpt(3))/MTvec(3);
-%             n2(3)=(zends(2)-MTpt(3))/MTvec(3);
-%             
-%             end1=MTpt-MTvec.*min(abs(n1));
-%             end2=MTpt-MTvec.*min(abs(n2));
-%             
-%         else
-%             n1(1)=(xends(2)-MTpt(1))/MTvec(1);
-%             n2(1)=(xends(1)-MTpt(1))/MTvec(1);
-%             n1(2)=(yends(1)-MTpt(2))/MTvec(2);
-%             n2(2)=(yends(2)-MTpt(2))/MTvec(2);
-%             n1(3)=(zends(1)-MTpt(3))/MTvec(3);
-%             n2(3)=(zends(2)-MTpt(3))/MTvec(3);
-%             
-%             end1=MTpt+MTvec.*min(abs(n1));
-%             end2=MTpt+MTvec.*min(abs(n2));
-%             
-%         end
-%         
-%     elseif xends(1)>MTpt(1) && xends(2)>MTpt(1)
-%         
-%         if(MTvec(1)>0)
-%             
-%             n1(1)=(xends(1)-MTpt(1))/MTvec(1);
-%             n2(1)=(xends(2)-MTpt(1))/MTvec(1);
-%             n1(2)=(yends(1)-MTpt(2))/MTvec(2);
-%             n2(2)=(yends(2)-MTpt(2))/MTvec(2);
-%             n1(3)=(zends(1)-MTpt(3))/MTvec(3);
-%             n2(3)=(zends(2)-MTpt(3))/MTvec(3);
-%             
-%             end1=MTpt-MTvec.*min(abs(n1));
-%             end2=MTpt-MTvec.*min(abs(n2));
-%             
-%         else
-%             n1(1)=(xends(2)-MTpt(1))/MTvec(1);
-%             n2(1)=(xends(1)-MTpt(1))/MTvec(1);
-%             n1(2)=(yends(1)-MTpt(2))/MTvec(2);
-%             n2(2)=(yends(2)-MTpt(2))/MTvec(2);
-%             n1(3)=(zends(1)-MTpt(3))/MTvec(3);
-%             n2(3)=(zends(2)-MTpt(3))/MTvec(3);
-%             
-%             end1=MTpt+MTvec.*min(abs(n1));
-%             end2=MTpt+MTvec.*min(abs(n2));
-%             
-%         end
+    %multipliers of MTvec to pass each plane
+    n1=v1./MTvec;
+    n2=v2./MTvec;
 
-    else
-        error('Cant handle this MT')        
-    end
-    
-    %one of the vectors needs to be flipped if the MTpt goes outside of the
-    %frame. This only works for it getting left behind in the x
-%     if xends(1) > 0 && MTpt(1)<xends(1)
-%         end1(1)=-end1(1);
-%     end
-%     
-%     if xends(1) < 0 && MTpt(1)>xends(1)
-%         end2(1)=-end2(1);
-%     end
-    
-%     if xends(1) > 0 && MTpt(1)>xends(1)
-%         end2(1)=-end2(1);
-%     end
+    %MT exits cube when it passes the closest plane
+    %find smallest multiplier
+    [~,I1]=min(abs(n1));
+    [~,I2]=min(abs(n2));
+
+    %end locations are MTvec*smallest multiplier
+    end2=MTpt+MTvec.*n1(I1);
+    end1=MTpt+MTvec.*n2(I2);
 
     if any(strcmp('Color',varargin))
         c=varargin{find(strcmp('Color',varargin))+1};
     else
         c='g';
     end
-    
+
     if any(strcmp('FaceAlpha',varargin))
         fa=varargin{find(strcmp('FaceAlpha',varargin))+1};
     else
         fa=.5;
     end
-    
+
     if any(strcmp('EdgeAlpha',varargin))
         ea=varargin{find(strcmp('EdgeAlpha',varargin))+1};
     else
         ea=.3;
     end
-    
+
     [h_cyl,h_cap1,h_cap2]=Cylinder(end1,end2,R_MT,20,c,0,ea,fa);
 
     if ~any(strcmp('EndLabelsOff',varargin))
@@ -159,9 +51,5 @@ function [ h_cyl,h_cap1,h_cap2 ] = draw_MT( xends,yends,zends,MTpt,MTvec,R_MT,va
             ...%,'HorizontalAlignment','right','VerticalAlignment','bottom')
             ,'HorizontalAlignment','center','VerticalAlignment','middle')
     end
-    
-    
-    %disp([end1(1)-.02*MTvec(1),end1(2)-.02*MTvec(2),end1(3)-.02*MTvec(3)])
 
 end
-
