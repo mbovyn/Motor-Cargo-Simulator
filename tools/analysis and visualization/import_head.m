a666f1fcf3cf42c17359b3e13814e55b362d21e0
Matt Bovyn <matt.bovyn@gmail.com>
2018-02-14 19:32:22 -0800

    Put import vars into struct to prevent conflicts

--- tools/analysis and visualization/import_head.m
+++ tools/analysis and visualization/import_head.m
@@ -10,32 +10,36 @@
 
 disp('Importing heads')
 
+headimport=struct;
+
 %% Initialize variables.
-filename = [localpath '/' run_name '_Heads.txt'];
-delimiter = ' ';
-startRow = 2;
+headimport.filename = [localpath '/' run_name '_Heads.txt'];
+headimport.delimiter = ' ';
+headimport.startRow = 2;
 
 %% Format string for each line of text:
 
 %need a %f for each column of numbers to read in
 %step, time, center x,y,z = 5
 %x,y,z for each motor = 3*(N_k+N_d)
-piece1=repmat('%f',1,3+3*(N(1)+N(2))+(N(1)+N(2)));
-piece2='%[^\n\r]';
+headimport.piece1=repmat('%f',1,3+3*(N(1)+N(2))+(N(1)+N(2)));
+headimport.piece2='%[^\n\r]';
 
-formatSpec=strcat(piece1,piece2);
+headimport.formatSpec=strcat(headimport.piece1,headimport.piece2);
 
 %% Open the text file.
-fileID = fopen(filename,'r');
+headimport.fileID = fopen(headimport.filename,'r');
 
 %% Read columns of data according to format string.
 % This call is based on the structure of the file used to generate this
 % code. If an error occurs for a different file, try regenerating the code
 % from the Import Tool.
-dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true, 'EmptyValue' ,NaN,'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+headimport.dataArray = textscan(headimport.fileID, headimport.formatSpec, ...
+    'Delimiter', headimport.delimiter, 'MultipleDelimsAsOne', true, ...
+    'EmptyValue' ,NaN,'HeaderLines' ,headimport.startRow-1, 'ReturnOnError', false);
 
 %% Close the text file.
-fclose(fileID);
+fclose(headimport.fileID);
 
 %% Post processing for unimportable data.
 % No unimportable data rules were applied during the import, so no post
@@ -44,45 +48,45 @@ fclose(fileID);
 % script.
 
 %% Allocate imported array to column variable names
-repeat = dataArray{:, 1};
-step = dataArray{:, 2};
-t_arr = dataArray{:, 3};
+repeat = headimport.dataArray{:, 1};
+step = headimport.dataArray{:, 2};
+t_arr = headimport.dataArray{:, 3};
 
 head_rec=cell(2,size(step,1));
 
 last=3+3*(N(1)+N(2))+(N(1)+N(2));
 
-bound{1}=[dataArray{:,last-(N(1)+N(2))+1:last-N(2)}];
-bound{2}=[dataArray{:,last-N(2)+1:last}];
+bound{1}=[headimport.dataArray{:,last-(N(1)+N(2))+1:last-N(2)}];
+bound{2}=[headimport.dataArray{:,last-N(2)+1:last}];
 
-%% 
+%%
 
-temp=cell(2,max(N));
+headimport.temp=cell(2,max(N));
 
-for m=1:2
-    
-    for n=1:N(m)
-        
-        ind=4+(m-1)*N(1)*3+(n-1)*3;
-        temp{m,n}=[dataArray{ind} dataArray{ind+1} ...
-            dataArray{ind+2}];
+for headimportm=1:2
+
+    for headimportn=1:N(headimportm)
+
+        headimport.ind=4+(headimportm-1)*N(1)*3+(headimportn-1)*3;
+        headimport.temp{headimportm,headimportn}=[headimport.dataArray{headimport.ind} ...
+            headimport.dataArray{headimport.ind+1} ...
+            headimport.dataArray{headimport.ind+2}];
 
     end
-    
+
 end
 
 %reformat to the shape motor_movie expects
-for m=1:2
-    temp2=zeros(N(m),3);
-    for s=1:size(step,1)
-        for n=1:N(m)
-            temp2(n,:)=temp{m,n}(s,:);
+for headimportm=1:2
+    headimport.temp2=zeros(N(headimportm),3);
+    for headimports=1:size(step,1)
+        for headimportn=1:N(headimportm)
+            headimport.temp2(headimportn,:)=headimport.temp{headimportm,headimportn}(headimports,:);
         end
-        head_rec{m,s}=temp2;
+        head_rec{headimportm,headimports}=headimport.temp2;
     end
 end
 
 
 %% Clear temporary variables
-clearvars ind temp temp2 m n step piece1 piece2...
-    filename delimiter startRow formatSpec fileID dataArray ans;
+clearvars headimport headimportm headimportn headimports;
