cb218feac889dbb0ab01738f6017bde2e0e4c4f1
Matt Bovyn <matt.bovyn@gmail.com>
2018-09-26 15:02:00 -0700

    Protect spaces in file paths in launch script

--- launching_scripts/launch.pl
+++ launching_scripts/launch.pl
@@ -6,17 +6,17 @@ use warnings;
 print "Launching (launch.pl)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
 
 #clean out old files
-system("$code_dir/launching_scripts/clean_files.sh $code_dir $working_dir $keep_old");
+system("'$code_dir'/launching_scripts/clean_files.sh '$code_dir' '$working_dir' $keep_old")==0 or die();
 
 #make parameter files
 do "$code_dir/launching_scripts/makeparams.pl" or die "$@ from call";
 
 if ($launch_mode eq 'local') { #launch simulations
-    system("$code_dir/launching_scripts/make_and_get_executable.sh $code_dir $working_dir $compile_keyword $compile") == 0
+    system("'$code_dir'/launching_scripts/make_and_get_executable.sh '$code_dir' '$working_dir' $compile_keyword $compile") == 0
         or die "compilation failed";
-    exec("$code_dir/launching_scripts/launch_over_paramfiles.sh '$run_name' '$code_dir' '$working_dir' $repeats $verbose $keep_seed $keep_old $dont_wait");
+    exec("'$code_dir'/launching_scripts/launch_over_paramfiles.sh '$run_name' '$code_dir' '$working_dir' $repeats $verbose $keep_seed $keep_old $dont_wait");
 } elsif ($launch_mode eq 'hpc') { #make parameter files only
-    system("$code_dir/launching_scripts/hpc/prepHPC.sh $code_dir $working_dir") == 0 or die "prep_hpc failed";
+    system("'$code_dir'/launching_scripts/hpc/prepHPC.sh '$code_dir' '$working_dir'") == 0 or die "prep_hpc failed";
     do "$code_dir/launching_scripts/hpc/make_pubs.pl" or die "$@ from call";
 } else {
     die "Invalid launch mode: should be 'local' or 'hpc'";
