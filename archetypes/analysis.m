a172c1d014ebc317303c2aef2dfb420d7e73f3b2
Matt Bovyn <matt.bovyn@gmail.com>
2018-10-15 17:56:21 -0700

    Add archetype for analyzing output

---
+++ archetypes/analysis.m
@@ -0,0 +1,12 @@
+%% setup for analyzing data
+
+%set names, paths and numbers of runs
+run_name={'condition1','condition2'};
+localpath={[pwd '/condition1directory'],[pwd '/condition2directory']};
+nruns={[10,1],[10,1]};
+
+%analysis file path
+analysispath='~/project_code/Motor_Freedom/analysis and visualization';
+
+%run import script
+run([analysispath '/import_params_and_results.m'])
