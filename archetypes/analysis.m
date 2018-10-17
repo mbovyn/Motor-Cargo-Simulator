b8dbf287bbfe492a21733edf8e2ef873e4ac4e96
Matt Bovyn <matt.bovyn@gmail.com>
2018-10-17 15:49:20 -0700

    Add note about import being conditional

--- archetypes/analysis.m
+++ archetypes/analysis.m
@@ -9,4 +9,5 @@ nruns={[10,1],[10,1]};
 analysispath='~/project_code/Motor_Freedom/analysis and visualization';
 
 %run import script
+%won't overwrite - only does import if variable params doesn't exist
 run([analysispath '/import_params_and_results.m'])
